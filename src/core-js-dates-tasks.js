ba8d4ef69bbe4c5f498c01de55c4f28b3fcdf8f8
Elina <elina50505051@yandex.ru>
2024-03-26 02:13:13 +0300

    feat: 4/13

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -65,8 +65,11 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const currentDay = date.getDay();
+  const nextFridayPeriod = 5 - currentDay > 0 ? 5 - currentDay : 12 - currentDay;
+  date.setDate(date.getDate() + nextFridayPeriod);
+  return date;
 }
 
 /**
