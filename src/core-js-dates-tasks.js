8f34d50a0416e165d6d05e08afac8d68639f89fd
Elina <elina50505051@yandex.ru>
2024-03-26 02:15:24 +0300

    feat: 5/13

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -97,8 +97,11 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const end = new Date(dateEnd).getTime();
+  const start = new Date(dateStart).getTime();
+  const diff = Math.abs(end - start);
+  return Math.floor(diff / (1000 * 3600 * 24)) + 1;
 }
 
 /**
